cdb68325f7bdbb5bb9a408a3706ae57c158e7e34
Juan Cuadra <jcb8793@gmail.com>
2018-12-05 13:34:35 -0600

    Basic Structure Done

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 
 /* Images */
 import logo from './assets/Logo.png';
-import cover from './assets/parallax/main-cover.png';
-import coverElements from './assets/parallax/main-cover-elements.png';
+import cover from './assets/parallax/Capa3_4.png';
+import hoverEl from './assets/parallax/Capa2_4.png';
+import scorllEl from './assets/parallax/Capa1_4.png';
 import story from './assets/story.png';
 import beauty from './assets/beauty.png';
 import immersive from './assets/immersive.png';
@@ -18,6 +19,26 @@ import aframe from './assets/tech/A-frame.png';
 import reactLogo from './assets/tech/React.png';
 import arkit from './assets/tech/arkit.png';
 
+import splotch from './assets/action/Why_Background.png';
+import geometry from './assets/action/geometry.png';
+
+/* Team */ 
+import team1 from './assets/team/team-1.png';
+import team2 from './assets/team/team-2.png';
+import team3 from './assets/team/team-3.png';
+import instagram from './assets/team/Instagram.png'
+
+/* Projects */
+import featured from './assets/projects/feature-project.png';
+
+/* Partners */
+import partners from './assets/partners/partners.png';
+
+/* Footer */
+import facebook from './assets/footer/facebook.png';
+import twitter from './assets/footer/twitter.png';
+import whiteLogo from './assets/footer/footer-logo.png';
+
 import './App.css';
 
 class App extends Component {
@@ -29,6 +50,7 @@ class App extends Component {
       coverElements: null
     }
     this.myRef = React.createRef();
+    this.what = React.createRef();
   }
 
   componentDidMount() {
@@ -48,11 +70,17 @@ class App extends Component {
     });
   }
 
+
+  handleScrollToElement(event) {
+    this.what.current.scrollIntoView({behaviour: 'smooth'})
+  }
+
+
   move = () => {
     const div =  document.getElementById("cover-elements");
     const moveByY = (this.props.position.y - div.offsetTop) + div.clientHeight/2;
     const moveByX = (this.props.position.x - div.offsetLeft);
-    div.style.transform = `translate(${moveByX/1000}%, ${moveByY/1000}%)`;
+    div.style.transform = `translate(${moveByX/800}%, ${moveByY/800}%)`;
   }
 
   render() {
@@ -61,10 +89,10 @@ class App extends Component {
         <header className="header">
           <img src={logo} className="App-logo" alt="logo" id="nav-logo" />
           <div id="main-menu">
-            <p>What we do</p>
-            <p>Who we are</p>
-            <p>Public projects</p>
-            <p>Our partners</p>
+            <p className="link" onClick={() => this.handleScrollToElement()}>What we do</p>
+            <p className="link">Who we are</p>
+            <p className="link">Public projects</p>
+            <p className="link">Our partners</p>
             <button>Contact us</button>
           </div>
         </header>
@@ -72,12 +100,13 @@ class App extends Component {
           <h1>We build VR experiences<br/>from your story</h1>
           <p>Whatever you are trying to say, let's say it together</p>
           <button>Learn more</button>
-          <div className="parallax-image-container">
+          <div className="parallax-image-container" onMouseMove={() => this.move()} >
             <img src={cover} className="cover-image" alt="cover" />
-            <img src={coverElements} onMouseMove={() => this.move()} id="cover-elements" alt="cover" style={{transform: 'translateY(' + this.state.transform + 'px)'}} ref={this.myRef}/>
+            <img src={hoverEl} id="cover-elements" alt="cover" ref={this.myRef}/>
+            <img src={scorllEl} alt="cover" style={{transform: 'translateY(' + this.state.transform + 'px)'}} ref={this.myRef}/>
           </div>
         </div>
-        <div id="why">
+        <div id="why" ref={this.what}>
           <h1>We use the emerging experience of VR<span></span></h1>
           <p>to create applications and interactions that help our partners <br/>transform their message into something unique.</p>
         </div>
@@ -117,6 +146,213 @@ class App extends Component {
             <img src={aframe} alt="aframe"/>
             <img src={reactLogo} alt="react"/>
             <img src={arkit} alt="arkit"/>
+          </div>
+        </div>
+        <div id="action">
+          <img src={splotch} alt="background" id="action-background"/>
+          <div className="purple-strip">
+            <img src={geometry} alt="details"/>
+          </div>
+          <div className="content">
+              <p>We want to build awesome things and we belive <br/> in the power of immersive technologies to deliver messages.</p>
+              <button>Show me how it works</button>
+          </div>
+        </div>
+        <div id="team">
+          <div className="description">
+            <h2>More than a company, <br/>we are a community.</h2>
+            <p>A team full of multi-disciplinary artsits, programmers and creatives <br/>that wanted to experiment with new mediums.</p>
+          </div>
+          <div className="profiles">
+            <div className="member">
+              <img src={team1} alt="team-member"/>
+              <div className="basic-info">
+                <h3>Juan Cuadra</h3>
+                <p>Pizzologo</p>
+              </div>
+              <div className="detail-profile">
+                <p className="first-line">Lorem Ipsum Bla bla bla</p>
+                <p>Lorem Ipsum Bla bla bla</p>
+                <p>Lorem Ipsum Bla bla bla</p>
+                <p>Lorem Ipsum Bla bla bla</p>
+                <p>Lorem Ipsum Bla bla bla</p>
+                <p>Lorem Ipsum Bla bla bla</p>
+                <p>Lorem Ipsum Bla bla bla</p>
+                <p>Lorem Ipsum Bla bla bla</p>
+                <p>Lorem Ipsum Bla bla bla</p>
+                <div className="social-icons">
+                  <img src={instagram} alt="instagram"/>
+                </div>
+              </div>
+            </div>
+            <div className="member">
+              <img src={team2} alt="team-member"/>
+              <div className="basic-info">
+                <h3>Juan Cuadra</h3>
+                <p>Lenguologo</p>
+              </div>
+              <div className="detail-profile">
+                <p className="first-line">Lorem Ipsum Bla bla bla</p>
+                <p>Lorem Ipsum Bla bla bla</p>
+                <p>Lorem Ipsum Bla bla bla</p>
+                <p>Lorem Ipsum Bla bla bla</p>
+                <p>Lorem Ipsum Bla bla bla</p>
+                <p>Lorem Ipsum Bla bla bla</p>
+                <p>Lorem Ipsum Bla bla bla</p>
+                <p>Lorem Ipsum Bla bla bla</p>
+                <p>Lorem Ipsum Bla bla bla</p>
+                <div className="social-icons">
+                  <img src={instagram} alt="instagram"/>
+                </div>
+              </div>
+            </div>
+            <div className="member">
+              <img src={team3} alt="team-member"/>
+              <div className="basic-info">
+                <h3>Juan Cuadra</h3>
+                <p>Risologo</p>
+              </div>
+              <div className="detail-profile">
+                <p className="first-line">Lorem Ipsum Bla bla bla</p>
+                <p>Lorem Ipsum Bla bla bla</p>
+                <p>Lorem Ipsum Bla bla bla</p>
+                <p>Lorem Ipsum Bla bla bla</p>
+                <p>Lorem Ipsum Bla bla bla</p>
+                <p>Lorem Ipsum Bla bla bla</p>
+                <p>Lorem Ipsum Bla bla bla</p>
+                <p>Lorem Ipsum Bla bla bla</p>
+                <p>Lorem Ipsum Bla bla bla</p>
+                <div className="social-icons">
+                  <img src={instagram} alt="instagram"/>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div id="projects">
+            <div className="title">
+              <h2>Our <br/>Projects</h2>
+            </div>
+            <div className="project-grid">
+              <div className="left-col">
+                <div className="featured-project">
+                  <img src={featured} alt="project-image"/>
+                  <div className="project-info">
+                    <div className="project-title">
+                      <p>Web VR</p>
+                      <h2>Project ABC</h2>
+                    </div>
+                    <div className="project-description">
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas maximus non ante sed lacinia. Sed nulla lorem, 
+                        accumsan sed libero quis, euismod fermentum nunc. Cras posuere ac est a porttitor. Aenean eget iaculis massa. 
+                        Praesent scelerisque libero laoreet, vulputate neque sit amet, viverra metus. Aenean ut lectus id lorem posuere 
+                        placerat eget ac neque. Nulla vel vehicula eros. In hac habitasse platea dictumst. Vivamus et vehicula augue. 
+                        Vestibulum lobortis nec metus at sodales. Pellentesque fringilla ultrices metus vitae fringilla. Praesent nec 
+                        volutpat ipsum. Integer finibus turpis nisi. Phasellus dapibus sapien quis molestie imperdiet.</p>
+                        <div className="button">
+                          <div className="button-background"></div>
+                          <div className="button-text">
+                            <p>Show me more</p>
+                            <p>→</p>
+                          </div>
+                        </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="third-project">
+                  <img src={featured} alt="project-image"/>
+                  <div className="project-info">
+                    <div className="project-title">
+                      <p>VR</p>
+                      <h2>Project VRFX</h2>
+                    </div>
+                    <div className="project-description">
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <div className="button">
+                          <div className="button-background"></div>
+                          <div className="button-text">
+                            <p>Show me more</p>
+                            <p>→</p>
+                          </div>
+                        </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="right-col">
+                <div className="second-project">
+                  <img src={featured} alt="project-image"/>
+                  <div className="project-info">
+                    <div className="project-title">
+                      <p>3D</p>
+                      <h2>Project ZYX</h2>
+                    </div>
+                    <div className="project-description">
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas maximus non ante sed lacinia. Sed nulla lorem, 
+                        accumsan sed libero quis, euismod fermentum nunc.</p>
+                        <div className="button">
+                          <div className="button-background"></div>
+                          <div className="button-text">
+                            <p>Show me more</p>
+                            <p>→</p>
+                          </div>
+                        </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="description">
+                  <h3>Begin your journey</h3>
+                  <p>Create 3D and VR experiences that engage your audience in a new and exciting way while delivering their messages as strongly as possible.</p>
+                  <div className="button">
+                    <div className="button-background teal"></div>
+                    <div className="button-text">
+                      <p>Show me more</p>
+                      <p>→</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        <div id="partners">
+          <h2>Our partners</h2>
+          <div className="logos-container">
+            <img src={partners} alt="partners"/>
+          </div>
+        </div>
+        <div id="footer">
+          <div id="footer-content">
+            <div className="footer-info">
+              <div id="social-media">
+                <h2>Let's make <br/>something great together!</h2>
+                <span></span>
+                <div className="social-logos">
+                  <img src={facebook} alt="facebook"/>
+                  <img src={twitter} alt="twitter"/>
+                  <img src={instagram} alt="instagram"/>
+                </div>
+              </div>
+              <div id="vitals-container">
+                <div className="vitals">
+                  <p>Meet</p>
+                  <h4>Managua, Nicaragua</h4>
+                </div>
+                <div className="vitals">
+                  <p>Write</p>
+                  <h4>hello@cromatico.io</h4>
+                </div>
+                <div className="vitals">
+                  <p>Talk</p>
+                  <h4>+(505) 8548-9394</h4>
+                </div>
+              </div>
+            </div>
+            <div className="footer-logo">
+              <img src={whiteLogo} alt="logo"/>
+              <div id="copyright">
+                <p>© 2018 Cromatico</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
